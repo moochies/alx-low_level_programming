ab9ea21a9c793f4ba553afdbeef6f34ffe0f6ff8
moochies <mutukukelvin99@gmail.com>
2023-08-23 06:56:18 -0700

    update

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,13 @@
+#include "main.h"
+char *_strchr(char *s, char c)
+{
+int i;
+for (i = 0; *(s + i); i++)
+{
+if (*(s + i) == c)
+return (s + i);
+}
+if (*(s + i) == c)
+return (s + i);
+return (0);
+}
